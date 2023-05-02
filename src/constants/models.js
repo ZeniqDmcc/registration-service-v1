052a2193475fe557b2d1bb8ae19ddb762c0673e5
Abdelfatah <zeniqdev@gmail.co>
2023-05-02 17:59:50 +0400

    updated-version-02/05/2023

---
+++ src/constants/models.js
@@ -0,0 +1,3 @@
+export const models = {
+    ZenconParticipant:"lap_zencon_participants"
+}
---
+++ src/middlewares/authentication.js
@@ -0,0 +1,28 @@
+import httpCodes from "../constants/httpCodes.js";
+import { sequelize,QueryTypes } from "../config/db.js";
+import apiResponse from "../utils/apiResponse.js";
+
+export default async (req,res,next)=>{
+    if(req.headers["hkey"] === process.env.HKEY){
+        return next();
+    }
+    const ethAddress = req.headers["token"];
+    if(!ethAddress){
+        return res.status(httpCodes.UNAUTHORIZED).json(apiResponse({errors:["Sorry!! Unauthorized access"]}));
+    }
+    const [results, metadata] = await sequelize.query(
+        "SELECT * FROM lap_comradewallets WHERE eth_wallet_address=$eth_wallet_address",
+        {
+        bind: {
+            eth_wallet_address:ethAddress,
+            type:QueryTypes.SELECT
+        }
+    });
+    if(!results.length){
+        return res.status(httpCodes.UNAUTHORIZED).json(apiResponse({errors:["Sorry!! Unauthorized access"]}));
+    }
+    req.eth_address = ethAddress;
+    req.session_id = results[0].comrade_id;
+    console.log(results)
+    return next()
+}
